0cc7814327ec2defa2543fe7044af410dc6ebbb9
FemFX <ignedko34@gmail.com>
2020-01-07 22:33:25 +0300

    Layouts

---
+++ src/layouts/EmptyLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="grey darken-1 empty-layout">
+    <router-view />
+  </div>
+</template>
+<script>
+import messages from '@/utils/messages'
+export default {
+  computed : {
+    error(){
+      return this.$store.getters.error
+    }
+  },
+  watch : {
+    error(firebaseError){
+      console.log(firebaseError);
+      this.$error(messages[firebaseError.code] || 'Что-то пошло не так')
+    }
+  }
+}
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app-main-layout">
+    <Navbar @click="isOpen = !isOpen"></Navbar>
+    <Sidebar v-model="isOpen" />
+    <main class="app-content" :class="{full : !isOpen}">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <router-link class="btn-floating btn-large blue" to="/record">
+        <i class="large material-icons">add</i>
+      </router-link>
+    </div>
+  </div>
+</template>
+<script>
+import Navbar from "@/components/app/Navbar";
+import Sidebar from "@/components/app/Sidebar";
+export default {
+  name: "main-layout",
+  data: () => ({
+    isOpen: true,
+  }),
+  async mounted() {
+    if (!Object.keys(this.$store.getters.info).length) {
+      await this.$store.dispatch("fetchInfo");
+    }
+  },
+  components: {
+    Navbar,
+    Sidebar
+  }
+};
+</script>
